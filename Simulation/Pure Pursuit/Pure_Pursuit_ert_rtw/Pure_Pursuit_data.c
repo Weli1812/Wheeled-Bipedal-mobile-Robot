7ea7662f25f8d751bba307e71dfe539b2515e305
cakeremix <Hassan.moahmed@student.giu-uni.de>
2026-03-16 15:40:47 +0200

    generated c code for pure pursuit

---
+++ Simulation/Pure Pursuit/Pure_Pursuit_ert_rtw/Pure_Pursuit_data.c
@@ -0,0 +1,42 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: Pure_Pursuit_data.c
+ *
+ * Code generated for Simulink model 'Pure_Pursuit'.
+ *
+ * Model version                  : 1.12
+ * Simulink Coder version         : 25.1 (R2025a) 21-Nov-2024
+ * C/C++ source code generated on : Mon Mar 16 15:35:27 2026
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex-M
+ * Code generation objectives:
+ *    1. Execution efficiency
+ *    2. RAM efficiency
+ * Validation result: Not run
+ */
+
+#include "Pure_Pursuit.h"
+
+/* Constant parameters (default storage) */
+const ConstP rtConstP = {
+  /* Expression: P
+   * Referenced by: '<Root>/Constant'
+   */
+  { 1.0, 1.3125255159679039, 2.8429307445631222, 3.259976699716737,
+    4.353158057842073, 4.483393807019195, 6.0125114391181214, 6.2457070354219031,
+    7.1926392682241049, 8.2397088722807226, 8.6345849146710911, 8.0, 1.0,
+    0.54539673909797082, 0.48181144010702837, 1.1466843318284137,
+    2.1916598255438191, 3.2051284414803858, 4.2055751483739234,
+    4.688137805796627, 6.1092068027935431, 6.8251781671710967, 8.347683115827218,
+    8.0 }
+};
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
--- Simulation/Pure Pursuit/untitled2.m
+++ Simulation/Pure Pursuit/untitled2.m
@@ -28,14 +28,15 @@ end
 P = path;
 
 %% Plot map and planned path
-figure
-show(map)
-hold on
+fig = figure;
+ax = axes(fig);
+show(map, 'Parent', ax)
+hold(ax, 'on')
 title('Robot Path Animation')
 
-plot(start(1), start(2), 'go', 'MarkerSize', 10, 'LineWidth', 2)
-plot(goal(1),  goal(2),  'rx', 'MarkerSize', 10, 'LineWidth', 2)
-plot(path(:,1), path(:,2), 'b--', 'LineWidth', 2)
+plot(ax, start(1), start(2), 'go', 'MarkerSize', 10, 'LineWidth', 2)
+plot(ax, goal(1),  goal(2),  'rx', 'MarkerSize', 10, 'LineWidth', 2)
+plot(ax, path(:,1), path(:,2), 'b--', 'LineWidth', 2)
 
 %% Get robot outputs from Simulink workspace object
 x_out     = out.x_out;
@@ -43,16 +44,52 @@ y_out     = out.y_out;
 theta_out = out.theta_out;
 tout      = out.tout;
 
+if isa(x_out, 'timeseries')
+    x_out = x_out.Data;
+end
+
+if isa(y_out, 'timeseries')
+    y_out = y_out.Data;
+end
+
+if isa(theta_out, 'timeseries')
+    theta_out = theta_out.Data;
+end
+
+if isa(tout, 'timeseries')
+    tout = tout.Data;
+end
+
+x_out = x_out(:);
+y_out = y_out(:);
+theta_out = theta_out(:);
+tout = tout(:);
+
+numSamples = min([numel(x_out), numel(y_out), numel(theta_out)]);
+
+if numSamples == 0
+    error('Simulation output is empty');
+end
+
+x_out = x_out(1:numSamples);
+y_out = y_out(1:numSamples);
+theta_out = theta_out(1:numSamples);
+
+% Render fewer frames for faster visualization on dense trajectories.
+maxRenderFrames = 500;
+frameStep = max(1, ceil(numSamples / maxRenderFrames));
+
 
 %% Create animated robot objects
-hRobot = plot(x_out(1), y_out(1), 'ro', ...
+hRobot = plot(ax, x_out(1), y_out(1), 'ro', ...
     'MarkerSize', 8, 'MarkerFaceColor', 'r');
 
-hTrail = plot(x_out(1), y_out(1), 'r', 'LineWidth', 2);
+hTrail = animatedline(ax, 'Color', 'r', 'LineWidth', 2);
+addpoints(hTrail, x_out(1), y_out(1));
 
 % Heading arrow
 arrowLength = 0.3;
-hArrow = quiver( ...
+hArrow = quiver(ax, ...
     x_out(1), y_out(1), ...
     arrowLength*cos(theta_out(1)), ...
     arrowLength*sin(theta_out(1)), ...
@@ -63,20 +100,38 @@ grid on
 axis equal
 
 %% Animation loop
-for k = 1:length(x_out)
-    % Update robot position
-    set(hRobot, 'XData', x_out(k), 'YData', y_out(k));
+for k = 1:frameStep:numSamples
+    if ~isgraphics(fig) || ~isgraphics(ax)
+        break;
+    end
 
-    % Update robot trail
-    set(hTrail, 'XData', x_out(1:k), 'YData', y_out(1:k));
+    if ~isgraphics(hRobot)
+        hRobot = plot(ax, x_out(k), y_out(k), 'ro', ...
+            'MarkerSize', 8, 'MarkerFaceColor', 'r');
+    else
+        hRobot.XData = x_out(k);
+        hRobot.YData = y_out(k);
+    end
 
-    % Update heading arrow
-    set(hArrow, ...
-        'XData', x_out(k), ...
-        'YData', y_out(k), ...
-        'UData', arrowLength*cos(theta_out(k)), ...
-        'VData', arrowLength*sin(theta_out(k)));
+    if ~isgraphics(hTrail)
+        hTrail = animatedline(ax, 'Color', 'r', 'LineWidth', 2);
+        addpoints(hTrail, x_out(1:k), y_out(1:k));
+    else
+        addpoints(hTrail, x_out(k), y_out(k));
+    end
 
-    drawnow
-    pause(0.01)
+    if ~isgraphics(hArrow)
+        hArrow = quiver(ax, ...
+            x_out(k), y_out(k), ...
+            arrowLength*cos(theta_out(k)), ...
+            arrowLength*sin(theta_out(k)), ...
+            0, 'k', 'LineWidth', 2);
+    else
+        hArrow.XData = x_out(k);
+        hArrow.YData = y_out(k);
+        hArrow.UData = arrowLength*cos(theta_out(k));
+        hArrow.VData = arrowLength*sin(theta_out(k));
+    end
+
+    drawnow limitrate nocallbacks
 end
