ce93bf07760367f53f0e345c9fcabfc5a8b2ad33
cakeremix <Hassan.moahmed@student.giu-uni.de>
2026-05-07 19:42:04 +0300

    trajectoryxPath planning code -> stm

---
+++ Hardware Testing/LQRxSTM/LQRxSTM_ert_rtw/ert_main.c
@@ -0,0 +1,109 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: ert_main.c
+ *
+ * Code generated for Simulink model 'LQRxSTM'.
+ *
+ * Model version                  : 1.27
+ * Simulink Coder version         : 25.2 (R2025b) 28-Jul-2025
+ * C/C++ source code generated on : Thu May  7 18:37:17 2026
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex-M
+ * Code generation objectives:
+ *    1. Execution efficiency
+ *    2. RAM efficiency
+ * Validation result: Not run
+ */
+
+#include "LQRxSTM.h"
+#include "rtwtypes.h"
+#include "MW_target_hardware_resources.h"
+
+volatile int IsrOverrun = 0;
+static boolean_T OverrunFlag = 0;
+void rt_OneStep(void)
+{
+  /* Check for overrun. Protect OverrunFlag against preemption */
+  if (OverrunFlag++) {
+    IsrOverrun = 1;
+    OverrunFlag--;
+    return;
+  }
+
+  __enable_irq();
+  LQRxSTM_step();
+
+  /* Get model outputs here */
+  __disable_irq();
+  OverrunFlag--;
+}
+
+volatile boolean_T stopRequested;
+volatile boolean_T runModel;
+int main(int argc, char **argv)
+{
+  float modelBaseRate = 0.005;
+  float systemClock = 480.0;
+
+  /* Initialize variables */
+  stopRequested = false;
+  runModel = false;
+
+#if !defined(MW_FREERTOS) && defined(MW_MULTI_TASKING_MODE) && (MW_MULTI_TASKING_MODE == 1)
+
+  MW_ASM (" SVC #1");
+
+#endif
+
+  ;
+
+  // Start Peripheral initialization imported from STM32CubeMX project;
+  HAL_Init();
+  SystemClock_Config();
+  PeriphCommonClock_Config();
+  MX_DMA_Init();
+  MX_GPIO_Init();
+  MX_TIM1_Init();
+  MX_TIM8_Init();
+  MX_USART1_UART_Init();
+  MX_USART2_UART_Init();
+
+  // End Peripheral initialization imported from STM32CubeMX project;
+  rtmSetErrorStatus(rtM, 0);
+  LQRxSTM_initialize();
+  __disable_irq();
+  ARMCM_SysTick_Config(modelBaseRate);
+  runModel =
+    rtmGetErrorStatus(rtM) == (NULL);
+  __enable_irq();
+  __enable_irq();
+  while (runModel) {
+    stopRequested = !(
+                      rtmGetErrorStatus(rtM) == (NULL));
+    if (stopRequested) {
+      SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
+    }
+
+    ;
+  }
+
+#if !defined(MW_FREERTOS) && !defined(USE_RTX)
+
+  (void) systemClock;
+
+#endif
+
+  ;
+  __disable_irq();
+  return 0;
+}
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
