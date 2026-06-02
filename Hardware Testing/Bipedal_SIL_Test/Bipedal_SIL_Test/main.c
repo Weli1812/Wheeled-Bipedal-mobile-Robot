d9e3faf0e8b6a62fb3a224d91527a46f7c89f8de
cakeremix <Hassan.moahmed@student.giu-uni.de>
2026-06-02 10:18:17 +0300

    Corrected PWM calculation

--- Hardware Testing/Bipedal_SIL_Test/Bipedal_SIL_Test/main.c
+++ Hardware Testing/Bipedal_SIL_Test/Bipedal_SIL_Test/main.c
@@ -55,7 +55,7 @@ int main() {
     InitKinematics(0.5f, 0.2f); 
     
     Pose start = {0.0f, 0.0f, 0.0f}; 
-    Pose goal = {45.0f, 0.0f, 0.0f}; 
+    Pose goal = {5.0f, 0.0f, 0.0f}; 
     Pose temp_path[MAX_PATH_LENGTH];
     
     InflateMap(global_map, inflated_map, 3); 
